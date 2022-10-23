07b1b65b67a3a4c8cc3dcd91ed32a394c4f3cbf4
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2022-10-23 10:56:52 -0700

    generate entire html page with export function

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -75,39 +75,39 @@ const generateTeam = team => {
         .join("")
     );
 
-    return html.join("";)
+    return html.join("");
 
 }
 
 
-
-
-
-`<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="./style.css">
-    <title>Team Profile Generator</title>
-</head>
-<body>
-    <header>
-        <nav id="navbar">
-            <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">My Team</span>
-        </nav>
-    </header>
-    <main>
-        <div class="container">
-            <div class="row justify-content-center" id="team-cards">
-                
-                    
-
+module.exports = team => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="./style.css">
+        <title>Team Profile Generator</title>
+    </head>
+    <body>
+        <header>
+            <nav id="navbar">
+                <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">My Team</span>
+            </nav>
+        </header>
+        <main>
+            <div class="container">
+                <div class="row justify-content-center" id="team-cards">
+                    ${generateTeam(team)}
+                </div>
             </div>
-        </div>
-    </main>
-</body>
-</html>`
+        </main>
+    </body>
+    </html>
+    `;
+};
+
