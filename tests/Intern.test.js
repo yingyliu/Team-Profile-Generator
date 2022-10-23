5351f1da4da918caa51255621fba23d8ee26ffba
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2022-10-23 16:24:11 -0700

    set up intern test

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,2 +1,16 @@
+const Intern = require ('../lib/Intern');
 
+test('creates an Intern object', () => {
+    const intern = new Intern('Ying', 79, 'ying@fakemail.com', 'UWA');
+    expect(inter.school).toEqual(expect.any(String));
+});
 
+test('get employee school', () => {
+    const intern = new Intern('Ying', 79, 'ying@fakemail.com', 'UWA');
+    expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
+});
+
+test("Testing role", () => {
+    const interrn = new Engineer('Ying', 79, 'ying@fakemail.com', 'UWA');
+    expect(intern.getRole()).toEqual("Intern");
+});
