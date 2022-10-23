032b4b8eeec354bfb23e059bb88d93c1bd281c6e
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2022-10-23 16:08:31 -0700

    set employee test info

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,2 +1,31 @@
+const Employee = require('../lib/Employee');
+
+test('creates an employee object', () => {
+    const employee = new Employee('Ying', 79, 'ying@fakemail.com');
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expece.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
+
+test('Testing name', () => {
+    const employee = new Employee('Ying', 79, 'ying@fakemail.com');
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+test('Testing ID', () => {
+    const employee = new Employee('Ying', 79, 'ying@fakemail.com');
+    expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+test('Testing email', () => {
+    const employee = new Employee('Ying', 79, 'ying@fakemail.com');
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+});
+
+test('Testing role', () => {
+    const employee = new Employee('Ying', 79, 'ying@fakemail.com');
+    expect(employee.getRole()).toEqual("Employee");
+});
+
 
 
