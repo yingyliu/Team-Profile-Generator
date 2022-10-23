1a3ffeaa4c68c5b0baac7b5ebe45a1ba16801e79
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2022-10-23 11:10:00 -0700

    move intern.test content to lib/intern.js

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,21 +1,2 @@
-const Employee = require('./Employee.test');
-
-class Intern extends Employee {
-    constructor (name, id, email, school) {
-        super (name, id, email);
-
-        this.school = school;
-    }
-
-    getSchool () {
-        return this.school;
-    }
-
-    getRole () {
-        return "Intern";
-    }
-}
-
-module.exports = Intern;
 
 
