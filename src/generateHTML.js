d0b2a8d45892648168c777af1e1328dea2d348bf
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2022-10-23 10:30:14 -0700

    paste index.html info to generateHTML.js

---
+++ src/generateHTML.js
@@ -0,0 +1,101 @@
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./style.css">
+    <title>Team Profile Generator</title>
+</head>
+<body>
+    <header>
+        <nav id="navbar">
+            <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">My Team</span>
+        </nav>
+    </header>
+    <main>
+        <div class="container">
+            <div class="row justify-content-center" id="team-cards">
+                
+                    <!-- Team Member(1)-->
+                <div class="col-4 mt-4">
+                    <div class="card border-dark mb-3" style="max-width: 18rem;">
+                        <div class="card-header">
+                            <h3>Jared</h3>
+                            <h4>Manager</h4><i class="fa-duotone fa-mug-hot"></i>
+                        </div>
+                        <div class="card-body text-dark">
+                        <p class="id">ID: 123</p>
+                        <p class="email">Email: <a href="mailto:jared@fakemail.com">jared@fakemail.com</a></p>
+                        <p class="office=">Office Number: 1</p>
+                        </div>
+                    </div>
+                </div>
+
+                    <!-- Team Member(2)-->
+                <div class="col-4 mt-4">
+                    <div class="card border-dark mb-3" style="max-width: 18rem;">
+                        <div class="card-header">
+                            <h3>Alec</h3>
+                            <h4>Engineer</h4><i class="fa-solid fa-glasses-round"></i>
+                        </div>
+                        <div class="card-body text-dark">
+                        <p class="id">ID: 234</p>
+                        <p class="email">Email: <a href="mailto:alec@fakemail.com">alec@fakemail.com</a></p>
+                        <p class="github">GitHub: <a href="https://github.com/yingyliu/Team-Profile-Generator">ibealec</a></p>
+                        </div>
+                    </div>
+                </div>
+
+                    <!-- Team Member(3)-->
+                <div class="col-4 mt-4">
+                    <div class="card border-dark mb-3" style="max-width: 18rem;">
+                        <div class="card-header">
+                            <h3>Grace</h3>
+                            <h4>Engineer</h4><i class="fa-solid fa-glasses-round"></i>
+                        </div>
+                        <div class="card-body text-dark">
+                        <p class="id">ID: 345</p>
+                        <p class="email">E-mail: <a href="mailto:grace@fakemail.com">grace@fakemail.com</a></p>
+                        <p class="github">GitHub: <a href="https://github.com/yingyliu/Team-Profile-Generator">gchoi2u</a></p>
+                        </div>
+                    </div>
+                </div>
+
+                    <!-- Team Member(4)-->
+                <div class="col-4 mt-4">
+                    <div class="card border-dark mb-3" style="max-width: 18rem;">
+                        <div class="card-header">
+                            <h3>Tammer</h3>
+                            <h4>Engineer</h4><i class="fa-solid fa-glasses-round"></i>
+                        </div>
+                        <div class="card-body text-dark">
+                        <p class="id">ID: 456</p>
+                        <p class="email">E-mail: <a href="mailto:tammer@fakemail.com">tammer@fakemail.com</a></p>
+                        <p class="github">GitHub: <a href="https://github.com/yingyliu/Team-Profile-Generator">tammerg</a></p>
+                        </div>
+                    </div>
+                </div>
+                    
+                    <!-- Team Member(5)-->
+                <div class="col-4 mt-4">
+                    <div class="card border-dark mb-3" style="max-width: 18rem;">
+                        <div class="card-header">
+                            <h3>John</h3>
+                            <h4>Intern</h4><i class="fa-solid fa-user-graduate"></i>
+                        </div>
+                        <div class="card-body text-dark">
+                        <p class="id">ID: 567</p>
+                        <p class="email">Email:<a href="mailto:john@fakemail.com">john@fakemail.com</a></p>
+                        <p class="school">School: 2University</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </main>
+</body>
+</html>`
