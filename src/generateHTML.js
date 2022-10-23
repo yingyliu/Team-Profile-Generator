a67ec6d39f0ca24c4c1998c2e055faa0ce368cc1
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2022-10-23 10:53:13 -0700

    create the team function in genereateHTML.js

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,3 +1,88 @@
+const generateTeam = team => {
+
+    const generateManager = manager => {
+        return `
+        <!-- Team Member-Manager-->
+        <div class="col-4 mt-4">
+        <div class="card border-dark mb-3" style="max-width: 18rem;">
+            <div class="card-header">
+                <h3>${manager.getName()}</h3>
+                <h4>${manager.getRole()}</h4><i class="fa-duotone fa-mug-hot"></i>
+            </div>
+            <div class="card-body text-dark">
+            <p class="id">ID: ${manager.getID()}</p>
+            <p class="email">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></p>
+            <p class="office=">Office Number: ${manager.getOfficeNumber()}</p>
+            </div>
+        </div>
+    </div>
+        `;
+    };
+
+    const generateEngineer = engineer => {
+        return `
+        <!-- Team Member-Engineer-->
+        <div class="col-4 mt-4">
+            <div class="card border-dark mb-3" style="max-width: 18rem;">
+                <div class="card-header">
+                    <h3>${engineer.getName()}</h3>
+                    <h4>${engineer.getRole()}</h4><i class="fa-solid fa-glasses-round"></i>
+                </div>
+                <div class="card-body text-dark">
+                <p class="id">ID: ${engineer.getID()}</p>
+                <p class="email">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></p>
+                <p class="github">GitHub: <a href="https://github.com/${engineer.getGithub()}">${engineer.getGithub()}</a></p>
+                </div>
+            </div>
+        </div>
+        `;
+    };
+
+    const generateIntern = intern => {
+        return `
+        <!-- Team Member(5)-->
+        <div class="col-4 mt-4">
+        <div class="card border-dark mb-3" style="max-width: 18rem;">
+            <div class="card-header">
+                <h3>${intern.getName()}</h3>
+                <h4>${intern.getRole()}</h4><i class="fa-solid fa-user-graduate"></i>
+            </div>
+            <div class="card-body text-dark">
+            <p class="id">ID: ${intern.getID()}</p>
+            <p class="email">Email:<a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></p>
+            <p class="school">School: ${intern.getSchool()}</p>
+            </div>
+        </div>
+        </div>
+        `;
+    };
+
+
+    const html = [];
+
+    html.push(team
+        .filter(employee => employee.getRole() === "Manager")
+        .map(manager => generateManager(manager))
+    );
+    html.push(team
+        .filter(employee => employee.getRole() === "Engineer")
+        .map(engineer => generateEngineer(engineer))
+        .join("")
+    );
+    html.push(team
+        .filter(employee => employee.getRole() === "Intern")
+        .map(intern => generateIntern(intern))
+        .join("")
+    );
+
+    return html.join("";)
+
+}
+
+
+
+
+
 `<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,80 +104,7 @@
         <div class="container">
             <div class="row justify-content-center" id="team-cards">
                 
-                    <!-- Team Member(1)-->
-                <div class="col-4 mt-4">
-                    <div class="card border-dark mb-3" style="max-width: 18rem;">
-                        <div class="card-header">
-                            <h3>Jared</h3>
-                            <h4>Manager</h4><i class="fa-duotone fa-mug-hot"></i>
-                        </div>
-                        <div class="card-body text-dark">
-                        <p class="id">ID: 123</p>
-                        <p class="email">Email: <a href="mailto:jared@fakemail.com">jared@fakemail.com</a></p>
-                        <p class="office=">Office Number: 1</p>
-                        </div>
-                    </div>
-                </div>
-
-                    <!-- Team Member(2)-->
-                <div class="col-4 mt-4">
-                    <div class="card border-dark mb-3" style="max-width: 18rem;">
-                        <div class="card-header">
-                            <h3>Alec</h3>
-                            <h4>Engineer</h4><i class="fa-solid fa-glasses-round"></i>
-                        </div>
-                        <div class="card-body text-dark">
-                        <p class="id">ID: 234</p>
-                        <p class="email">Email: <a href="mailto:alec@fakemail.com">alec@fakemail.com</a></p>
-                        <p class="github">GitHub: <a href="https://github.com/yingyliu/Team-Profile-Generator">ibealec</a></p>
-                        </div>
-                    </div>
-                </div>
-
-                    <!-- Team Member(3)-->
-                <div class="col-4 mt-4">
-                    <div class="card border-dark mb-3" style="max-width: 18rem;">
-                        <div class="card-header">
-                            <h3>Grace</h3>
-                            <h4>Engineer</h4><i class="fa-solid fa-glasses-round"></i>
-                        </div>
-                        <div class="card-body text-dark">
-                        <p class="id">ID: 345</p>
-                        <p class="email">E-mail: <a href="mailto:grace@fakemail.com">grace@fakemail.com</a></p>
-                        <p class="github">GitHub: <a href="https://github.com/yingyliu/Team-Profile-Generator">gchoi2u</a></p>
-                        </div>
-                    </div>
-                </div>
-
-                    <!-- Team Member(4)-->
-                <div class="col-4 mt-4">
-                    <div class="card border-dark mb-3" style="max-width: 18rem;">
-                        <div class="card-header">
-                            <h3>Tammer</h3>
-                            <h4>Engineer</h4><i class="fa-solid fa-glasses-round"></i>
-                        </div>
-                        <div class="card-body text-dark">
-                        <p class="id">ID: 456</p>
-                        <p class="email">E-mail: <a href="mailto:tammer@fakemail.com">tammer@fakemail.com</a></p>
-                        <p class="github">GitHub: <a href="https://github.com/yingyliu/Team-Profile-Generator">tammerg</a></p>
-                        </div>
-                    </div>
-                </div>
                     
-                    <!-- Team Member(5)-->
-                <div class="col-4 mt-4">
-                    <div class="card border-dark mb-3" style="max-width: 18rem;">
-                        <div class="card-header">
-                            <h3>John</h3>
-                            <h4>Intern</h4><i class="fa-solid fa-user-graduate"></i>
-                        </div>
-                        <div class="card-body text-dark">
-                        <p class="id">ID: 567</p>
-                        <p class="email">Email:<a href="mailto:john@fakemail.com">john@fakemail.com</a></p>
-                        <p class="school">School: 2University</p>
-                        </div>
-                    </div>
-                </div>
 
             </div>
         </div>
